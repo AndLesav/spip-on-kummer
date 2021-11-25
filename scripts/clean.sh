b974aa8c3b67ec391678a4db7a7984d3056f770a
LESAVOUREY Andrea <andrea.lesavourey@irisa.fr>
2021-11-25 19:03:24 +0000

    Upload New File

---
+++ scripts/clean.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm -f kf_units_*;
+rm -f kf_eval_geo_*;
+rm -f kf_attack_*;
+rm -f kf_enum_cost_*;
+
+rm -f ke_units_*;
+rm -f ke_eval_geo_*;
+rm -f ke_attack_*;
+rm -f ke_enum_cost_*;
+
+exit 0;
