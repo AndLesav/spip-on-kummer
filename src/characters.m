af47342c1dc34cf9b248a6775682499f3b9c91a0
LESAVOUREY Andrea <andrea.lesavourey@irisa.fr>
2021-11-25 18:56:27 +0000

    Upload New File

---
+++ src/characters.m
@@ -0,0 +1,152 @@
+EnoughCharacters := function(U, field_sequence, field_exponent) /* Calculate p-adic characters and put the result into a matrix */
+    Q := [];
+    field_length := #field_sequence;
+    field_dim := field_exponent^field_length;
+    r := #U;
+    C := [1 : i in [1..field_length]];
+    M := [];
+
+    b_f, b_quo := KF_primefree_basis(field_sequence, field_exponent);
+
+    q := 2;
+    while (#Q lt Round(3*r)+Round(2*Sqrt(r))+field_dim) do
+	/* "finding one prime"; */
+	q := One_good_prime(field_sequence, C, q, field_exponent);
+	F_q := GF(q);
+
+	basis_q := KF_naive_basis_q_new(field_sequence, field_exponent, F_q);
+	t := 0;
+	E := [];
+	for i in [1..r] do
+	    /* "calculating embedding"; */
+	    e := Embedding_q(U[i], field_sequence, field_exponent, F_q, basis_q: basis_vector := b_quo);
+	    if (e eq 0) then
+		break;
+	    else
+		t := t + 1;
+		Append(~E, e);
+	    end if;
+	end for;
+	
+	if (t eq r) then	/* test if q is good for every element in U */
+	    Q1 := Include(Q, q);
+	    if Q1 ne Q then	/* test if the good q hasn't been chosen before */
+		Q := Q1;
+		zeta := RootOfUnity(field_exponent, F_q);
+		Char_q := [ E[i]^((q-1) div field_exponent) : i in [1..r] ];
+		Row_q := [ Log(zeta, (Char_q)[j]) : j in [1..r] ];
+		Append(~M, Row_q);
+	    end if;
+	end if;   
+    end while;
+    return Matrix(GF(field_exponent), M);
+end function;
+
+
+
+/* calculate p-adic characters -- relative version */
+Relative_EnoughCharacters := function(U, ground_field, extension)
+    Q := [];
+    ground_length := #ground_field[1];
+    ext_length := #extension[1];
+    ext_dim := extension[2]^ext_length;
+    r := #U;
+    C_ground := [1 : i in [1..ground_length]];
+    C_ext := [1 : i in [1..ext_length]];
+    M := [];
+    q := 2;
+    /* while (#Q lt (r + 5*ext_dim+ground_length^ground_field[2])) do */
+    while (#Q lt Round(3*r)+Round(2*Sqrt(r))+ext_dim+ground_length^ground_field[2]) do
+	q := Relative_one_good_prime(ground_field, extension, C_ground, C_ext, q);
+	F_q := GF(q);
+	basis_q := Relative_naive_basis_q(ground_field, extension, F_q);
+	t := 0;
+	E := [];
+	for i in [1..r] do
+	    e := Relative_embedding_q(U[i], ground_field, extension, F_q:basis:=basis_q);
+	    if (e eq 0) then
+		break;
+	    else
+		t := t + 1;
+		Append(~E, e);
+	    end if;
+	end for;
+	if (t eq r) then /* test if q is good for every element in U */
+	    Q1 := Include(Q, q);
+	    //Round(Log(2, q));
+	    if Q1 ne Q then         /* test if the good q hasn't been chosen before */
+		//F_q := GF(q);
+		Q := Q1;
+		if extension[2] eq 2 then
+		    zeta := F_q!(-1);
+		else
+		    zeta := RootOfUnity(extension[2], F_q);
+		end if;
+		Char_q := [ E[i]^((q-1) div extension[2]) : i in [1..r] ];
+		Row_q := [ Log(zeta, (Char_q)[j]) : j in [1..r] ];
+		Append(~M, Row_q);
+	    end if;
+	end if;   
+    end while;
+    return Matrix(GF(extension[2]), M);
+end function;
+
+
+
+KF_multifinite_morphism := function(U, field_sequence, field_exponent) /* Calculate good morphism for U from K to F_p1 x ...x F_pr */
+    Q := [];
+    field_length := #field_sequence;
+    field_dim := field_exponent^field_length;
+    r := #U;
+    V := [[] : i in [1..#U]];
+    C := [1 : i in [1..field_length]];
+    M := [];
+    //q := RandomPrime(5);
+    q := 2;
+    while (#Q lt (r + 30)) do
+	/* "finding one prime"; */
+	q := One_good_prime(field_sequence, C, q, field_exponent);
+	F_q := GF(q);
+	/* "calculating basis"; */
+	basis_q := KF_naive_basis_q(field_sequence, field_exponent, F_q);
+	t := 0;
+	E := [];
+	for i in [1..r] do
+	    /* "calculating embedding"; */
+	    e := Embedding_q(U[i], field_sequence, field_exponent, F_q, basis_q);
+	    if (e eq 0) then
+		break;
+	    else
+		t := t + 1;
+		Append(~E, e);
+	    end if;
+	end for;
+	
+	if (t eq r) then			/* test if q is good for every element in U */
+	    Q1 := Include(Q, q);
+	    if Q1 ne Q then			/* test if the good q hasn't been chosen before */
+		Q := Q1;
+		for i in [1..#E] do
+		    Append(~V[i], Rationals()!(Integers()!E[i]));
+		end for;
+	    end if;
+	end if;   
+    end while;
+    return V, Q;
+end function;
+
+
+/* p-adic characters of U */
+KF_characters_from_field := function(U, Q, field_sequence, field_exponent)
+    M := [];
+    r := #U;
+    for i in [1..#Q] do
+	q := Q[i];
+	F_q := GF(q);
+	zeta := RootOfUnity(field_exponent, F_q);			/* calculate zeta_p in F_q and Log(zeta_p, CubeCharacter_q) */
+	Char_q := [(F_q!U[j, i])^((q-1) div field_exponent) : j in [1..r]];
+	Row_q := [ Log(zeta, (Char_q)[j]) : j in [1..r] ];
+	Append(~M, Row_q);
+    end for;
+    return Matrix(GF(field_exponent), M);
+end function;
