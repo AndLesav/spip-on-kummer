8bf863687bcb89245d821b7c4688746141d0cf92
LESAVOUREY Andrea <andrea.lesavourey@irisa.fr>
2021-11-25 19:03:00 +0000

    Update start_file.m

--- src/start_file.m
+++ src/start_file.m
@@ -1,4 +1,3 @@
-load "../MyLLL.m";
 load "../src/general.m";
 load "../src/kummer_structure.m";
 load "../src/good_primes.m";
@@ -10,7 +9,6 @@ load "../src/kummer_units.m";
 load "../src/kummer_ideals.m";
 load "../src/keygen.m";
 load "../src/general_fields_functions.m";
-/* load "../src/maximal_order.m"; */
 load "data_and_tests.m";
 
 Pol_ring<x> := PolynomialRing(Integers());
