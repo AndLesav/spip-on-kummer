adadac842de0c03eff5112a7e3273691e7fdbac2
LESAVOUREY Andrea <andrea.lesavourey@irisa.fr>
2021-11-25 19:02:32 +0000

    Upload New File

---
+++ src/start_file.m
@@ -0,0 +1,17 @@
+load "../MyLLL.m";
+load "../src/general.m";
+load "../src/kummer_structure.m";
+load "../src/good_primes.m";
+load "../src/characters.m";
+load "../src/field_structure.m";
+load "../src/embeddings.m";
+load "../src/real_root.m";
+load "../src/kummer_units.m";
+load "../src/kummer_ideals.m";
+load "../src/keygen.m";
+load "../src/general_fields_functions.m";
+/* load "../src/maximal_order.m"; */
+load "data_and_tests.m";
+
+Pol_ring<x> := PolynomialRing(Integers());
+
