9668d7b6032192815358a4ae435416b956c6fa43
AndLesav <95079499+AndLesav@users.noreply.github.com>
2021-11-30 22:17:04 +0100

    Update data_and_tests.m

--- scripts/data_and_tests.m
+++ scripts/data_and_tests.m
@@ -1,18 +1,18 @@
-// SPIP on Kummer
-   // Copyright (C) 2021  LESAVOUREY Andrea
+/*  SPIP on Kummer
+    Copyright (C) 2021  LESAVOUREY Andrea
 
-    // This program is free software: you can redistribute it and/or modify
-    // it under the terms of the GNU General Public License as published by
-    // the Free Software Foundation, either version 3 of the License, or
-    // (at your option) any later version.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-    // This program is distributed in the hope that it will be useful,
-    // but WITHOUT ANY WARRANTY; without even the implied warranty of
-    // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    // GNU General Public License for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-    // You should have received a copy of the GNU General Public License
-    // along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
 
 
 KF_stat_log_norm_key := function(kummer_field, size_key, precision: size_set:=200, U:=[], pruning:=false, p:=1/2, n:=10, shape:="uni", supp:=1, file:="")
